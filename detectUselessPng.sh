67fbd2377c95123fc06a98b108dfe1511f43f789
Major-Eth <major-tom.eth@pm.me>
2022-04-06 16:38:38 +0200

    feat: reduce image based on max width

--- detectUselessPng.sh
+++ detectUselessPng.sh
@@ -2,7 +2,7 @@ for f in ./public/_posts/**/**/**.png ./public/_posts/**/**/**/**.png;
 do 
 	value=$(convert $f -channel a -format "%[mean]" info:) ;
 	if [ $value == 0 ]; then
-		magick $f ${f/%.png/.jpg} 
+		# magick $f ${f/%.png/.jpg} 
 		echo ${f/%.png/.jpg};
 	fi;
 done;
