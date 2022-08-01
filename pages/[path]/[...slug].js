77ff399c7abcff45b3224b6e832119d0ff54b401
qosmonot <32738380+qosmonot@users.noreply.github.com>
2022-08-01 17:40:57 +0300

    add path for "draper"

--- pages/[path]/[...slug].js
+++ pages/[path]/[...slug].js
@@ -30,6 +30,7 @@ export async function getStaticProps({params, locale}) {
 		(params.path === 'articles' && (params.slug.length === 1 && params.slug[0] === 'people-of-yearn')) ||
 		(params.path === 'articles' && (params.slug.length === 1 && params.slug[0] === 'kish')) ||
 		(params.path === 'articles' && (params.slug.length === 1 && params.slug[0] === 'farrahmay')) ||
+		(params.path === 'articles' && (params.slug.length === 1 && params.slug[0] === 'draper')) ||
 		(params.path === 'articles' && (params.slug.length === 1 && params.slug[0] === 'transcriptions')) ||
 		(params.path === 'articles' && (params.slug.length === 1 && params.slug[0] === 'yearn-finance')) ||
 		(params.path === 'updates' && (params.slug.length === 1 && params.slug[0] === 'web-team'))		
@@ -120,6 +121,7 @@ export async function getStaticPaths() {
 		paths.push({params: {path: 'articles', slug: ['people-of-yearn']}, locale: code});
 		paths.push({params: {path: 'articles', slug: ['kish']}, locale: code});
 		paths.push({params: {path: 'articles', slug: ['farrahmay']}, locale: code});
+		paths.push({params: {path: 'articles', slug: ['draper']}, locale: code});
 		paths.push({params: {path: 'articles', slug: ['transcriptions']}, locale: code});
 		paths.push({params: {path: 'updates', slug: ['web-team']}, locale: code});		
 	});
